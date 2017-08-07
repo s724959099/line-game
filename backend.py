ad1f922f8656b85143814002935382c2bff3830c
meatball <mb0520@sinyi-tech.com>
2017-08-07 21:06:09 +0800

    test

--- backend.py
+++ backend.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 
 @app.route("/", methods=['POST', "GET"])
 def index():
-    app.logger.info("test~~~~ logger")
-    print("print~~~~~")
+    channel_access_token = os.environ.get("channel_access_token", "YOUR_CHANNEL_ACCESS_TOKEN")
+    channel_secret = os.environ.get("channel_secre", "YOUR_CHANNEL_SECRET")
+    print("toke=",channel_access_token)
+    print("channel_secret=", channel_secret)
     return "Text"
 
 
