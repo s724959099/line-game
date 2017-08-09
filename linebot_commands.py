b2be4ce2ca9468cbb418a7931e64a8fa37f8af3b
meatball <mb0520@sinyi-tech.com>
2017-08-09 22:17:34 +0800

    to heroku

--- linebot_commands.py
+++ linebot_commands.py
@@ -16,9 +16,29 @@ invoker = Invoker()
 invoker.appends(commands)
 game = GameDB.get_instance()
 
-a = []
+import json
+
+FILE_NAME = "db.json"
+
+
+def write_json(d):
+    with open(FILE_NAME, "w") as f:
+        json.dump(d, f)
+
+
+def read_json():
+    try:
+        with open(FILE_NAME) as f:
+            return json.load(f)
+    except:
+        return {}
 
 
 def events_excute(event):
     line = LineAPI(event)
+    db = read_json()
+    db.setdefault("a", [])
+    line.reply("len={}".format(db["a"]))
+    db["a"].append(1)
+    write_json(db)
     # invoker.execute(execute_all=True, event=event, line=line, game_db=game)
