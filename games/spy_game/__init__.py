0c3b5b01bc7d6dbe8d3f530daab0f35d9cde6a61
meatball <mb0520@sinyi-tech.com>
2017-08-09 17:35:35 +0800

    to heroku

--- games/spy_game/__init__.py
+++ games/spy_game/__init__.py
@@ -8,21 +8,24 @@ def template(title, msg, messages):
 
 
 def init_game(line, event, game_db):
-    if game_db.in_group(event.source.group_id):
-        game_db.get_room(event.source.group_id).game = SpyGame()
-        line.reply(template(
-            "遊戲建立中",
-            "參加玩家回答完後回答好了即可開始",
-            [
-                "我",
-                "好了",
-                "遊戲人數",
-            ]
-        ))
-        # line.reply("debugger 中 in group init game")
-    else:
-        game_db.debugger_rooms(line)
-        # awake_bot(line, event, game_db)
+    try:
+        if game_db.in_group(event.source.group_id):
+            game_db.get_room(event.source.group_id).game = SpyGame()
+            line.reply(template(
+                "遊戲建立中",
+                "參加玩家回答完後回答好了即可開始",
+                [
+                    "我",
+                    "好了",
+                    "遊戲人數",
+                ]
+            ))
+            # line.reply("debugger 中 in group init game")
+        else:
+            game_db.debugger_rooms(line)
+            # awake_bot(line, event, game_db)
+    except Exception as e:
+        line.reply(str(e))
 
 
 def show_spy(line, event, game_db):
