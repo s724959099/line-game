3868072eb7e7871a3340e12375db46e2571283b3
meatball <mb0520@sinyi-tech.com>
2017-08-10 21:18:56 +0800

    to heroku

--- utli/db.py
+++ utli/db.py
@@ -76,6 +76,7 @@ class Model:
     def __item_init(cls, item):
         if isinstance(item, dict) and item.get("__class"):
             print("module=", item.get("__module"))
+            print("class=", item.get("__class"))
             m = __import__(item.get("__module"), globals(), locals(), [""])
             sub_cls = getattr(m, item.get("__class"))
             result = sub_cls.init(item)
