3f66402d43394c2318ce3a1b6cb344f04f8d9be1
meatball <mb0520@sinyi-tech.com>
2017-08-16 22:59:33 +0800

    增加角色

--- games/spy_game/game.py
+++ games/spy_game/game.py
@@ -9,14 +9,23 @@ class SpyGame(db.Model):
 
     def init_images(self):
         self.postion_images = [
-            ("醫院", "https://i.imgur.com/ffgqsjU.png"),
-            ("十字軍東征", "https://i.imgur.com/mIJZQkE.jpg"),
-            ("海灘", "https://i.imgur.com/mRd9Hjb.jpg"),
-            ("學校", "https://i.imgur.com/SVrPdGO.jpg"),
-            ("演唱會", "https://i.imgur.com/PvyJ7rP.jpg"),
-            ("月球", "https://i.imgur.com/UbBRqhS.jpg"),
-            ("阿爾卑斯山", "https://i.imgur.com/5j0CHPP.jpg"),
-            ("陰屍路", "https://i.imgur.com/hYrb1Pb.jpg"),
+            ("醫院", "https://i.imgur.com/ffgqsjU.png",
+             ["俏護士", "怪醫黑傑克", "兩光醫生", "帶兩串香蕉探病的病人家屬", "頭髮骨折的病人", "院長", "志工", "保全", "水電工", "外送小弟"]),
+            ("十字軍東征", "https://i.imgur.com/mIJZQkE.jpg",
+             ["弓箭手", "你是一隻馬", "騎士", "教宗", "雜兵", "高級雜兵", "不知道哪裡來的傭兵", "躺在地上假死的士兵", "聖職人員", "步兵", "主教", "農民"]),
+            ("海灘", "https://i.imgur.com/mRd9Hjb.jpg",
+             ["比基尼辣妹", "比丘尼", "鮪魚線的男人", "救生員", "很猛的猛男", "小孩子", "潛水員", "剛讀大學的小Q醬"]),
+            ("學校", "https://i.imgur.com/SVrPdGO.jpg",
+             ["數學老師", "小混混", "英文老師", "教務主任", "訓導主任", "糾察隊", "風紀股長", "電器股長", "no function 吸％等畢業的學生", "籃球隊隊長隔壁的球隊經理",
+              "籃球隊隊長"]),
+            ("演唱會", "https://i.imgur.com/PvyJ7rP.jpg",
+             ["賣黃牛票的小弟", "高雄金城武", "高雄鹽埕區", "李光洙", "范范范曉宣", "范范范尾鰭", "小粉絲", "工作人員"]),
+            ("月球", "https://i.imgur.com/UbBRqhS.jpg",
+             ["嫦娥", "火星人", "科學家", "阿姆斯壯", "月兔", "太空戰士", "太空人"]),
+            ("阿爾卑斯山", "https://i.imgur.com/5j0CHPP.jpg",
+             ["登山客", "阿爾卑斯山的小女孩", "大雪怪", "大腳怪", "雪女"]),
+            ("陰屍路", "https://i.imgur.com/hYrb1Pb.jpg",
+             ["弓箭手", "手拿消防栓的人", "喪屍", "狼師", "拿拐杖的老人", "拿棒棒糖的小孩", "警察"]),
         ]
         self.spy_image = ("間諜", "https://i.imgur.com/K9R7i8R.jpg")
 
@@ -65,6 +74,7 @@ class SpyGame(db.Model):
             else:
                 line.push(profile["user_id"], text_message(self.picker_position[0]))
                 line.push(profile["user_id"], image_message(self.picker_position[1]))
+                line.push(profile["user_id"], image_message("您這場的角色為: {}".format(self.picker_position[2])))
 
     def show_game(self, group_id, line):
         if self.users is None:
