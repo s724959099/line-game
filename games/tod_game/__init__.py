39e05816e325fec648074e318f0c91dd7424ff11
meatball <mb0520@sinyi-tech.com>
2017-09-24 15:44:25 +0800

    tod game init

--- games/tod_game/__init__.py
+++ games/tod_game/__init__.py
@@ -5,13 +5,34 @@ from games.game_enviroment import *
 import random
 import re
 
+
 def template(title, msg, messages):
     url = 'https://i.imgur.com/K9R7i8R.jpg'
     return base_template(title, msg, url, messages)
 
 
-def to_start(line, event, game_db):
+def function_template(line, event, game_db):
+    room = game_db.get_room(event.source.group_id)
+    game = room.game
+    # game.users = room.users[:]
 
+
+def end_gmae(line, event, game_db):
+    # 這樣就結束遊戲了
+    room = game_db.get_room(event.source.group_id)
+    room.game = None
+
+
+def in_final_code(line, event, game_db):
+    room = game_db.get_room(event.source.group_id)
+    game = room.game
+    game.in_final_code(line,event)
+
+
+def start_game(line, event, game_db):
+    room = game_db.get_room(event.source.group_id)
+    game = room.game
+    game.start_game(room.users[:])
     line.push(event.source.group_id, template(
         "title",
         "msg",
@@ -22,128 +43,42 @@ def to_start(line, event, game_db):
     ))
 
 
-def random_peo(line, event, game_db):
+def choose_game(line, event, game_db):
     room = game_db.get_room(event.source.group_id)
-
     game = room.game
-    game.users = room.users[:]
-    game.random_user(line)
-    line.push(event.source.group_id, template(
-        "title",
-        "msg",
-        messages=[
-            "真心話",
-            "大冒險"
-        ]
-    ))
-
-
-def end_gmae(line, event, game_db):
-    room = game_db.get_room(event.source.group_id)
-    # room.game.show_game(event.source.group_id, line)
-    room.game = None
-
-
-def restart_gmae(line, event, game_db):
-    room = game_db.get_room(event.source.group_id)
-    room.game = TodGame()
-    to_start(line, event, game_db)
-
-
-def finalcode(line, event, game_db, pw=None):
-    room = game_db.get_room(event.source.group_id)
-
-    game = room.game
-
-    tod_game.finelcode_user()
-
-
-    # game.users = room.users[:]
-    # game.random_user(line)
-
-
-
-
-
-
-
-def final_pw(line, event, game_db, pw=None):
-    room = game_db.get_room(event.source.group_id)
-
-    game = room.game
-    # game.users = room.users[:]
-    # game.random_user(line)
-
-
-
-    if(tod_game.finel_pw(line, event))
-
-        line.push(event.source.group_id, template(
-            "title",
-            "msg",
-            messages=[
-                "真心話",
-                "大冒險"
-            ]
-        ))
-
-
-def true_talk(line, event, game_db):
-    room = game_db.get_room(event.source.group_id)
-    tod_game = room.game
-    # tod_game.play = (room.users[:], line)
-
-    if(tod_game.truth_talk(line, event)):
-        line.push(event.source.group_id, template(
-            "title",
-            "msg",
-            ["結束遊戲", "重新開始", "遊戲大廳"]
-        ))
-
-
-def adventure(line, event, game_db):
-
-    room = game_db.get_room(event.source.group_id)
-    tod_game = room.game
-
-    if(tod_game.adventure(line, event)):
-        line.push(event.source.group_id, template(
-            "title",
-            "msg",
-            ["結束遊戲", "重新開始", "遊戲大廳"]
-        ))
+    if event.message.text == "電腦隨機":
+        game.choose_loser(line)
+    if event.message.text == "終極密碼":
+        game.to_final_game()
 
 
 def game_lobby(line, event, game_db):
-    append_group(line, event, game_db)
-    # room = game_db.get_room(event.source.group_id)
-    # if room.game == None:
-    #     append_group(line, event, game_db)
-    # else:
-    #     line.push(event.source.group_id, template(
-    #         "",
-    #         "請按下方結束遊戲",
-    #         ["結束遊戲"]
-    #     ))
+    # todo 大廳要顯示什麼 未來寫在base_commadns
+    room = game_db.get_room(event.source.group_id)
+    tod_game = room.game
+
+
+def choose_punishment(line, event, game_db):
+    room = game_db.get_room(event.source.group_id)
+    game = room.game
+    if game.is_game_over():
+        game.choose_punishment(line, event, event.message.text)
+        line.push(event.source.group_id,
+                  template("遊戲已結束", "請選擇", ["結束遊戲", "遊戲大廳", "重新開始"]))
 
 
 def commands():
-
-
     return [
 
-        SimpleCommandFactory(to_start, "OK"), #1
-        SimpleCommandFactory(restart_gmae, "重新開始"),  # 1
+        SimpleCommandFactory(start_game, "OK"),  # 1
+        SimpleCommandFactory(start_game, "重新開始"),  # 1
 
-        SimpleCommandFactory(random_peo, "電腦隨機"),  #2
-        SimpleCommandFactory(finalcode, "終極密碼"),   #2
-        SimpleCommandFactory(final_pw, no_if=True), #3
-        SimpleCommandFactory(true_talk, "真心話"), #4
-        SimpleCommandFactory(adventure, "大冒險"), #4
+        SimpleCommandFactory(choose_game, "電腦隨機"),  # 2
+        SimpleCommandFactory(choose_game, "終極密碼"),  # 2
+        SimpleCommandFactory(in_final_code, no_if=True),  # 3
+        SimpleCommandFactory(choose_punishment, "真心話"),  # 4
+        SimpleCommandFactory(choose_punishment, "大冒險"),  # 4
 
-
-        SimpleCommandFactory(end_gmae, "結束遊戲"), #5
-        SimpleCommandFactory(game_lobby, "遊戲大廳"), #
+        SimpleCommandFactory(end_gmae, "結束遊戲"),  # 5
+        SimpleCommandFactory(game_lobby, "遊戲大廳"),  #
     ]
-
-
--- games/tod_game/game.py
+++ games/tod_game/game.py
@@ -1,164 +1,115 @@
 from utli.base_import import *
 import re
 
+adventure_db = [
+    "跟旁邊的女生要賴(如果沒有就跟群組的人要)",
+    "大喊我愛大咪咪(我愛大香腸)",
+    "把鞋子拿起來當電話(說您撥的電話無人回應)",
+    "拿一個人的襪子(沒穿就拿隔壁的)玩擠眉弄眼",
+    "拿手機播一首哥走一圈(邊做動作邊走)",
+    "打電話跟一個異性朋友告白",
+    "去跟不認識的人自我介紹然後一起聊天(5分鐘)",
+    "抱一個異性(ex：公主抱...)",
+    "跟異性講冷笑話(可以上網查或指定)",
+    "對不認識的人唱歌用嘴巴含水(飲料)直到對方猜出來",
 
+]
+truth_talk_db = [
+    "現在有沒有喜歡的人",
+    "玩遊戲中的人有你喜歡的人嗎",
+    # ("現在有沒有喜歡的人","玩遊戲中的人有你喜歡的人嗎"),
+    "第一次接吻幾歲",
+    "有沒有過一夜情",
+    "有沒有劈腿過",
+    "分享一個另一半做過最印象深刻(感動)的事",
+    "想生幾個小孩",
+    "最想感謝的人跟為什麼",
+    "未來的夢想",
+    "如果時間能重來最想做什麼"
+
+]
+
+
+class Status:
+    choose_game = 1
+    in_final_code = 2
+    game_lose = 3
+    game_over = 4
+    game_end = 5
 
 
 class TodGame(db.Model):
     FILE = __file__
 
-
-
-    def __init__(self, trigger_text = None):
+    def __init__(self):
         self.users = []
-        self.speical_man = None
-        self.tod_users = []
-        self.picker_position = None
-        self.finel_user = 0
-        self.init_data()
-        self.reply_index = 0
-
+        self.loser = None
 
         self.fipw = random.randint(1, 99)
         self.pw_head = 0
         self.pw_tail = 100
+        self.boom = None
 
-        self.status = 1
+        # 遊戲狀態
+        self.status = Status.choose_game
+        self.reply_index = 0
 
-        # self.__text = trigger_text
-        # if isinstance(self.__text, str):
-        #     self.__text = [self.__text]
-        # self.__text = list(map(lambda x: x.lower(), self.__text))
+        # 單純覺得很擾亂我而已所以把他移上去因為不常看到
+        # 真心話
+        self.truth_talk_db = adventure_db
+        # 大冒險
+        self.adventure_db = adventure_db
 
+    def choose_loser(self, line):
+        if self.status == Status.choose_game:
+            # 跳過遊戲 直接從狀態2變成4
+            self.loser = random.choice(self.users)
+            line.push(event.source.group_id, "{} lose".format(
+                loser["display_name"]
+            ))
+            self.status = Status.game_over
 
+    def to_final_game(self):
+        self.status = Status.in_final_code
+        self.boom = random.randint(1, 99)
 
+    def __in_range(self, count):
+        return self.pw_head < count < self.pw_tail
 
-    def init_data(self):
-        self.truth_talk_db = [
-            "現在有沒有喜歡的人",
-            "玩遊戲中的人有你喜歡的人嗎",
-            # ("現在有沒有喜歡的人","玩遊戲中的人有你喜歡的人嗎"),
-            "第一次接吻幾歲",
-            "有沒有過一夜情",
-            "有沒有劈腿過",
-            "分享一個另一半做過最印象深刻(感動)的事",
-            "想生幾個小孩",
-            "最想感謝的人跟為什麼",
-            "未來的夢想",
-            "如果時間能重來最想做什麼"
-
-        ]
-        self.adventure_db = [
-            "跟旁邊的女生要賴(如果沒有就跟群組的人要)",
-            "大喊我愛大咪咪(我愛大香腸)",
-            "把鞋子拿起來當電話(說您撥的電話無人回應)",
-            "拿一個人的襪子(沒穿就拿隔壁的)玩擠眉弄眼",
-            "拿手機播一首哥走一圈(邊做動作邊走)",
-            "打電話跟一個異性朋友告白",
-            "去跟不認識的人自我介紹然後一起聊天(5分鐘)",
-            "抱一個異性(ex：公主抱...)",
-            "跟異性講冷笑話(可以上網查或指定)",
-            "對不認識的人唱歌用嘴巴含水(飲料)直到對方猜出來",
-
-
-        ]
-
-    def random_user(self, line):
-        self.speical_man = choose_list(self.users, 1)[0]
-        line.reply('the man is {}'.format(self.speical_man["display_name"]))
-
-    def finelcode_user(self, event, line):
-
-
-
-
-
-
-
-        # print("range {0}-{1}".format(pw_head, pw_tail))
-        line.reply("range {0}-{1}".format(self.pw_head, self.pw_tail))
-
-        # cp.p(line.profile["display_name"] + "請輸入密碼: ", cp.colors.red)
-
-
-        line.push(event.source.group_id, "{} 請輸入密碼：".format(user_list[self.reply_index]))
-        # line.push(event.source.group_id, "{} 請輸入密碼：".format(user["display_name"]))
-        # line.reply("{} 請輸入密碼：".format(user["display_name"]) )
-
-
-
-
-
-
-
-
-    def finel_pw(self, line, event):
-
-
-
+    def in_final_code(self, line, event):
+        if self.status == Status.in_final_code:
             input_msg = event.message.text
-
-
             if re.compile(r'^[0-9]+$').match(input_msg):
-            # if input_msg.isdigit() :
-                if int(input_msg) > self.pw_head and int(input_msg) < self.pw_tail and int(input_msg) != self.pw:
-                    return int(input_msg)
-                elif int(input_msg) == self.pw:
-                    self.speical_man = line.profile
-                    return int(input_msg)
+                if not self.__in_range(input_msg):
+                    line.push(event.source.group_id, "請輸入範圍內數字")
                 else:
-                    # print("Please input number({}-{})!!!!".format(pw_head, pw_tail))
-                    line.reply("Please input number({}-{})!!!!".format(self.pw_head, self.pw_tail))
+                    if self.boom == input_msg:
+                        line.push(event.source.group_id, "你輸了")
+                        self.status = Status.game_over
+                    else:
+                        temp = self.pw_head
+                        self.pw_head = input_msg
+                        if not self.__in_range(self.boom):
+                            self.pw_head = temp
+                            self.pw_tail = input_msg
 
-                    # cp.p(line.profile["display_name"] + "請輸入密碼: ", cp.colors.red)
+                        line.push(event.source.group_id, "{} - {}".format(
+                            self.pw_head, self.pw_tail
+                        ))
 
-                    # line.reply(user['display_name'] + "請輸入密碼：")
-                    # input_msg = input()
+    def is_game_over(self):
+        return self.status == Status.game_over
 
-            else:
-                # print("Please can only input number \n range({}-{})!!!!".format(pw_head, pw_tail))
-                line.reply("Please can only input number \n range({}-{})!!!!".format(self.pw_head, self.pw_tail))
+    def start_game(self, users):
+        self.__init__()
+        self.users = users
 
-                # cp.p(line.profile["display_name"] + "請輸入密碼: ", cp.colors.red)
-                # line.reply(user['display_name'] + "請輸入密碼：")
-
-                # input_msg = input()
-
-
-
-
-
-    def truth_talk(self, line, event):
-        self.picker_position = random.choice(self.truth_talk_db)
-
-        if event.source.user_id == self.speical_man["user_id"]:
-            line.push(event.source.group_id, text_message("Ｑ：" + self.picker_position))
-            return True
-
-
-
-    def adventure(self, line, event):
-        self.picker_position = random.choice(self.adventure_db)
-
-        if event.source.user_id == self.speical_man["user_id"]:
-            line.push(event.source.group_id, text_message("Ｑ：" + self.picker_position))
-            return True
-
-    def commands(self):
-        return [
-            print('spm')
-
-        ]
-
-
-
-def commands():
-
-    print('a')
-
-    return[
-
-        print('spm')
-
-
-    ]
+    def choose_punishment(self, line, event, choice):
+        choice_list = {
+            '真心話': 'truth_talk_db',
+            '大冒險': 'adventure_db',
+        }
+        if choice_list.get(choice) is not None and self.status == Status.game_over:
+            punishment_list = getattr(self, choice_list.get(choice))
+            line.push(event.source.group_id, punishment_list.choice())
+            self.status = Status.game_end
